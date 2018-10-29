b32e3ac05482c6031ec9144bf0016b9d1c4710a7
Alex Garcia and Brooke Perry <alex@>
2018-10-29 16:23:00 -0700

    add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
-function PlacesVisited() {
-  this.places = []
-}
 
-PlacesVisited.prototype.addPlaces = function(place) {
-    this.places.push(place);
-}
+var placesArray = [];
+console.log(placesArray);
+
+// function PlacesVisited() {
+//   this.places = []
+// }
+// //
+// PlacesVisited.prototype.addPlaces = function(vacay) {
+//     this.places.push(vacay);
+// }
 
 function Location(state,landmarks,date,description) {
   this.state = state,
@@ -13,6 +17,21 @@ function Location(state,landmarks,date,description) {
   this.description = description
 }
 
-$(document).ready(function(
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+debugger;
+  var state = $("#state").val();
+  var landmark = $("#landmark").val();
+  var date = $("#date").val();
+  var description = $("#description").val();
+  var vacay = new Location(state,landmark,date,description);
 
-))
+  placesArray.push(vacay);
+  //PlacesVisited
+
+ });
+
+});
+
+//$("#results-show").append("<li>" + placesArray[i].landmarks + "</li>")
